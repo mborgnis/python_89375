71d574415fb762cb735c7f20d682d5293078c04f
inventario_ex <micuborg@gmail.com>
2026-04-26 21:29:00 -0300

    desarrollo de templates

--- inicio/urls.py
+++ inicio/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path #Esto llama al archivo settings para django
-from inicio.views import inicio
+from django.urls import path #Esto llama al archivo settings > urls
+from inicio.views import inicio, productos #Esto llama a la funcion que esta en inicio > views
 
 app_name='inicio'
 
 urlpatterns = [
-    path('', inicio, name='inicio')
+    path('', inicio, name='inicio'),
+    path('productos/', productos, name='productos'),
 ]
--- inicio/views.py
+++ inicio/views.py
@@ -2,5 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+
+# VISTA UNO
+#def inicio(request):
+#    return HttpResponse('Esta es tu primera vista de inicio')
+
 def inicio(request):
-    return HttpResponse('Esta es tu primera vista de inicio')
+    return render(request, "inicio/inicio.html", {})
+
+def productos(request):
+    return render(request, "inicio/productos.html", {})
